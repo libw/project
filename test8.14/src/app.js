50adb60f22f2844ab53974c1220559fc13d24331
libw <3180234769@qq.com>
2017-08-14 12:51:06 +0800

    libw
    1

---
+++ test8.14/src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router,Route,IndexRoute,hashHistory} from 'react-router';
+import ff from'./rr.css'
+
+
+
+
+class App extends React.Component{
+	render(){
+		return(
+			<div className={ff.gg}>	
+				353535
+			</div>				
+		)
+	}
+}
+
+ReactDOM.render((
+	<div className={ff.gg}>
+				1111353535
+			</div>
+	),document.body
+);
+
